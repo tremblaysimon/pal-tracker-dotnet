nwade/pal-tracker-dotnet-v2

Add manifest file for deployment to PCF

src/PalTracker/EnvController.cs
@@ -1,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PalTracker
+{
+    [Route("env")]
+    public class EnvController : Controller
+    {
+        private readonly CloudFoundryInfo _cloudFoundryEnv;
+
+        [HttpGet]
+        public CloudFoundryInfo Get() => _cloudFoundryEnv;
+
+        public EnvController(CloudFoundryInfo cloudFoundryEnv)
+        {
+            _cloudFoundryEnv = cloudFoundryEnv;
+        }
+    }
+}

tremblaysimon/pal-tracker-dotnet

src/PalTracker/EnvController.cs
@@ -1,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PalTracker
+{
+    [Route("env")]
+    public class EnvController : ControllerBase
+    {
+        private readonly CloudFoundryInfo _cloudFoundryEnv;
+
+        [HttpGet]
+        public CloudFoundryInfo Get() => _cloudFoundryEnv;
+
+        public EnvController(CloudFoundryInfo cloudFoundryEnv)
+        {
+            _cloudFoundryEnv = cloudFoundryEnv;
+        }
+    }
+}
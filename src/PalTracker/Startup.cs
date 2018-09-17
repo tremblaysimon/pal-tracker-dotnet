3b8b480e945b71b37e960c62a059b026da24e097
Simon Tremblay <tremblaysimon@gmail.com>
2018-09-17 17:06:26 -0400

    rest api

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -34,6 +34,7 @@ namespace PalTracker
                Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX not configured."),
                Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR not configured.")
          ));
+         services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
